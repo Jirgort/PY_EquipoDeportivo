7517a4abf6892c6d2036b7b777fc4d448557f8ea
Jirgort <Jirgort@gmail.com>
2022-12-22 14:25:13 -0600

    Arreeglo de errores

--- appThresholdcr/src/app/components/adminModules/coach-management/coach-management.component.ts
+++ appThresholdcr/src/app/components/adminModules/coach-management/coach-management.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-coach-management',
   templateUrl: './coach-management.component.html',
   styleUrls: ['./coach-management.component.css'],
 })
 export class CoachManagementComponent {
-  constructor(private router: Router) {}
-
+  constructor(private router: Router,private formBuilder: FormBuilder,) {}
+  registerForm=this.formBuilder.group({
+    nombre: ['', Validators.required],
+    nameUser: ['', Validators.required],
+    password: ['', Validators.required],
+    age: [Validators.required],
+    weight: [Validators.required],
+    height: ['', Validators.nullValidator],
+  })
   submit() {
     this.router.navigate(['/adminHome']);
 
